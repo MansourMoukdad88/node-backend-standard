aac8551cb9698bd7ff9e626c17acad8efcac4914
MansourMoukdad88 <m_sy@outlook.com>
2020-04-29 00:15:44 +0300

    [Add] Place-Specific Routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,40 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous shy scrapers in the world!",
+    imageUrl:
+      "https://res.cloudinary.com/drbgxq5pt/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fwww.kitano.com%2Fresourcefiles%2Fsnippet-main-img%2Fempire-state-building-in-new-york-top.jpg%3Fmimdevice%3Dmobile",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp!!! State Building",
+    description: "One of the most famous shy scrapers in the world!",
+    imageUrl:
+      "https://res.cloudinary.com/drbgxq5pt/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fwww.kitano.com%2Fresourcefiles%2Fsnippet-main-img%2Fempire-state-building-in-new-york-top.jpg%3Fmimdevice%3Dmobile",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+];
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1}
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place }); // => { place: place}
 });
 
 module.exports = router;
